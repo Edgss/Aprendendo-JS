48df896685026586efae48b49630785be0e07af1
Edgss <eduardoogomess211@gmail.com>
2025-03-17 21:04:29 -0300

    atualização exercicios

--- Exercicios/Descontos/script.js
+++ Exercicios/Descontos/script.js
@@ -1,7 +1,7 @@
 alert("Estamos com ofertas de 10% em todos os produtos, calcule já o seu:")
 var preco = Number(prompt("Qual o valor do produto ?"))
-var desconto = preço * 0.10
-var precoFinal = preço - desconto
+var desconto = preco * 0.10
+var precoFinal = preco - desconto
 
 
 document.writeln(`O valor final do produto com o desconto é de ${precoFinal.toLocaleString("pt-br", {style:"currency", currency: "BRL"})}`)
